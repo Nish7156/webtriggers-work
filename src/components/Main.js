1c3ac00d776d4d340775394d7fc60f2e45d520bc
Nish7156 <shedagenishant40@gmail.com>
2023-10-18 12:13:46 +0530

    fix layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Main() {
   return (
-    <div>Main</div>
+    <>
+       <div class="gallery-items sf_true">
+                            <div class="gallery-items-container fl-wrap">
+                                <div class="gallery-item nature">
+                                    <div class="hov_box">
+                                        <img src="images/folio/22.jpeg" alt=""/>
+                                        <div class="overlay"></div>
+                                        <div class="hov_box-title">
+                                            <a href="portfolio-single.html" class="ajax">Car Design</a>
+                                            <div class="cat_item">Cars</div>
+                                        </div>
+                                    </div>
+                                </div>
+                             
+                            </div>
+                        </div>  
+    </>
   )
 }
 
